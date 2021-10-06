ac4338f3d2f96b1f5dd5afabd5bfac7cee54546c
dukey1221 <rubenestor816@gmail.com>
2021-10-06 22:30:35 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PeopleComponent } from './people-rnd.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
